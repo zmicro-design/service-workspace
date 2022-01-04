0f0ebf89e618039409846d429b8a8ecebe276fbf
whatwewant <tobewhatwewant@outlook.com>
2022-01-04 22:23:27 +0800

    fix(docker): fail2ban and rsyslogd failed to start

--- image/entrypoint.sh
+++ image/entrypoint.sh
@@ -26,12 +26,20 @@ sudo service ssh restart
 if [ -f /etc/init.d/rsyslog ]; then
   # Reference: https://stackoverflow.com/questions/22526016/docker-container-sshd-logs
   # echo "Start Rsyslog(/var/log/auth.log) ..."
+  if [ -f "/run/rsyslogd.pid" ]; then
+    sudo rm -rf /run/rsyslogd.pid
+  fi
+
   sudo service rsyslog restart
 fi
 
 if [ -f /etc/init.d/fail2ban ]; then
   # Reference: https://stackoverflow.com/questions/22526016/docker-container-sshd-logs
   # echo "Start Fail2ban(/var/log/fail2ban.log) ..."
+  if [ -f "/var/run/fail2ban/fail2ban.sock" ]; then
+    sudo rm -rf /var/run/fail2ban/fail2ban.sock
+  fi
+
   sudo service fail2ban restart
 fi
 
